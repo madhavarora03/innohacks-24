31bb81ef5d43756441c92e1c41c3cbd53ace0f19
madhav <eragon.cube@outlook.com>
2024-03-06 23:42:51 +0530

    Adding user controller

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import authRoute from './auth.route';
 import healthCheckRoute from './healthcheck.route';
+import orderRoute from './order.route';
 import productRoute from './product.route';
 import reviewRoute from './review.route';
-import orderRoute from './order.route';
+import userRoute from './user.route';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/healthcheck', healthCheckRoute);
 router.use('/products', productRoute);
 router.use('/reviews', reviewRoute);
 router.use('/orders', orderRoute);
+router.use('/users', userRoute);
 
 export default router;
