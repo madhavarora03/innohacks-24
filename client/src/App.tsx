cc52d18f6326395f617525ff5881082a65e38bd9
Ayush Thakur <atindia7@gmail.com>
2024-03-07 00:37:58 +0530

    Added Functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
+import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import { RootState } from './lib/store';
 
 function App() {
+  const { value } = useSelector((state: RootState) => state.themeReducer);
+  document.querySelector('html')?.setAttribute('data-theme', value);
   return (
     <>
       <Header />
--- client/src/routes/Cart/index.tsx
+++ client/src/routes/Cart/index.tsx
@@ -1,32 +1,75 @@
-import { useSelector } from "react-redux";
+import { CrossIcon, Trash2 } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { removeFromCart } from "../../lib/slices/cartSlice";
 import { RootState } from "../../lib/store";
 
 function CartPage() {
-    const { cartItems, paymentMethod, shippingAddress } = useSelector((state: RootState) => state.cart);
+    const { cartItems } = useSelector((state: RootState) => state.cart);
     return <div className="m-24">
-        {JSON.stringify(cartItems)}
+        {/* {JSON.stringify(cartItems)} */}
         <div className="block md:flex ">
-            <CartItems items={cartItems} />
-            <CheckoutScreen />
+            {cartItems.length ? <CartItems items={cartItems} /> : <div role="alert" className="alert alert-error">
+                <CrossIcon />
+                <span>Add some items to the cart to view</span>
+            </div>}
+            <CheckoutScreen items={cartItems} />
         </div>
     </div>
 }
 
 function CartItems({ items }: any) {
-    return <div className="overflow-auto">
-        <table>
-            <thead>
-
+    const dispatch = useDispatch();
+    return <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-1/2">
+        <h1 className="font-extrabold underline text-2xl  text-center">Current Items in the cart</h1>
+        <table className="w-full text-sm text-left rtl:text-right table">
+            <thead className="text-xs uppercase">
+                <tr>
+                    <th scope="col" className="px-6 py-3"></th>
+                    <th scope="col" className="px-6 py-3">
+                        Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Image
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        <span className="sr-only">Delete</span>
+                    </th>
+                </tr>
             </thead>
-            {items.map((item: any) => {
-                return <div key={item.id}>{item.name}</div>
-            })}
+            <tbody>
+                {items.map((item: any, index: number) => {
+                    return <tr>
+                        <th scope="row" className="px-6 py-4">{index + 1}</th>
+                        <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap text-lg">
+                            {item.name}
+                        </th>
+                        <td className="px-6 py-4">
+                            <img src={item.image} alt={item.name} width={128} height={128} />
+                        </td>
+                        <td className="px-6 py-4 text-right">
+                            <button className="btn btn-error btn-outline" onClick={() => dispatch(removeFromCart(item.id))}><Trash2 /></button>
+                        </td>
+                    </tr>
+                })}
+            </tbody>
         </table>
-    </div>;
+    </div>
+
 }
 
-function CheckoutScreen() {
-    return <div></div>;
+function CheckoutScreen({ items }: any) {
+    var price: number = 0;
+    items.forEach((item: any) => price += item.price);
+    return <div className="w-1/2  flex justify-end">
+        <div className="border w-fit p-8 rounded-xl text-center flex flex-col gap-4">
+            <h1 className="text-3xl font-extrabold">Subtotal</h1>
+            <span className="text-xl underline ">
+                Subtotal: {price}
+            </span>
+            <Link to={'/checkout'}><button className="btn w-full btn-outline btn-primary">Checkout</button></Link>
+        </div>
+    </div>;
 }
 
 export default CartPage;
